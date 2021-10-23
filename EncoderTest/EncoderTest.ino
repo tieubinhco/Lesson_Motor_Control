5fc26d3da64d9e0e7a89592aefe0349b5946a7c7
Trần Tiểu Bình <tieubinhco@gmail.com>
2021-10-23 20:19:20 +0700

    Initial commit

---
+++ EncoderTest/EncoderTest.ino
@@ -0,0 +1,36 @@
+#include <Encoder.h>
+
+Encoder motorLeft(2, 3);
+Encoder motorRight(7, 8);
+//   avoid using pins with LEDs attached
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Two motor Encoder Test:");
+}
+
+long positionLeft  = -999;
+long positionRight = -999;
+
+void loop() {
+  long newLeft, newRight;
+  newLeft = motorLeft.read();
+  newRight = motorRight.read();
+  if (newLeft != positionLeft || newRight != positionRight) {
+    Serial.print("Left = ");
+    Serial.print(newLeft);
+    Serial.print(", Right = ");
+    Serial.print(newRight);
+    Serial.println();
+    positionLeft = newLeft;
+    positionRight = newRight;
+  }
+  // if a character is sent from the serial monitor,
+  // reset both back to zero.
+  if (Serial.available()) {
+    Serial.read();
+    Serial.println("Reset both motor to zero");
+    knobLeft.write(0);
+    knobRight.write(0);
+  }
+}
