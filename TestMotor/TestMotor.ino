a6ef784844622bed5d0d640d236ff0141f3b24e3
Trần Tiểu Bình <tieubinhco@gmail.com>
2021-10-23 20:54:50 +0700

    Create TestMotor.ino

---
+++ TestMotor/TestMotor.ino
@@ -0,0 +1,20 @@
+#define IN1 7
+#define IN2 8
+#define ENA 5
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(IN1,OUTPUT);
+  pinMode(IN2,OUTPUT);
+  pinMode(ENA,OUTPUT);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(IN1,LOW);
+  digitalWrite(IN2,HIGH);
+  analogWrite(ENA,255);
+
+}
