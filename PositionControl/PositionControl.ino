4f2ecc824f76b0502587f2ea1126cfdbb8df5894
Trần Tiểu Bình <tieubinhco@gmail.com>
2021-10-25 20:38:25 +0700

    Run ok, Kp is too much

--- PositionControl/PositionControl.ino
+++ PositionControl/PositionControl.ino
@@ -5,7 +5,7 @@
 #define IN2 8
 
 Encoder motor(2,3);
-long pos=0;
+long currentPos=0;
 
 float desiredAngle=90;
 float currentAngle=0; 
@@ -18,10 +18,66 @@ void setup() {
   pinMode(ENA,OUTPUT);
   pinMode(IN1,OUTPUT);
   pinMode(IN2,OUTPUT);
+  Serial.begin(9600);
+  Serial.println("Rotate to the desired angle using proportional controller");
+  Serial.println("Press r to set current position to 0 degree");
+  while(true)
+  {
+    char startingChar=Serial.read(); 
+    delay(500);
+    Serial.print(".");
+    if (startingChar=='r')
+    {
+        Serial.println();
+        break;
+    }
+  }
+  motor.write(0);
+  Serial.println("Current position is set to 0 degree");
+  Serial.println("Press s to start");
+  while(true)
+  {
+    char startingChar=Serial.read(); 
+    delay(500);
+    Serial.print(".");
+    if (startingChar=='s')
+    {
+        Serial.println();
+        break;
+    }
+  }
+  Serial.println("Program started");
+}
+
+float convert_to_degree(long currentPulse)
+{
+  float revolution = float(float(currentPulse)/float(194));
+  float degree= revolution * 360;
+  return degree;
 }
 
 void loop() {
-//set zero angle
-while 
+
+currentPos=motor.read();
+currentAngle=convert_to_degree(currentPos);
+Serial.print("Degree= \t");
+Serial.println(int(currentAngle));
+
+err=desiredAngle-currentAngle;
+pwmValue=Kp*err;
+direct=0;
+
+if (pwmValue<0)
+{
+  pwmValue=abs(pwmValue);
+  direct=1;
+}
+
+if (pwmValue<30) pwmValue=30;
+else if(pwmValue>255) pwmValue=255;
+
+digitalWrite(IN1,direct);
+digitalWrite(IN2,!direct);
+analogWrite(ENA,pwmValue);
 
 }
