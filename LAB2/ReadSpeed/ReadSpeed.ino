5e191e46e419daee00bc904e348eb84cca9a0c28
Tieu Binh <tieubinhco@gmail.com>
2021-11-01 22:41:45 +0700

    Create ReadSpeed.ino

---
+++ LAB2/ReadSpeed/ReadSpeed.ino
@@ -0,0 +1,51 @@
+#include <Encoder.h>
+#define IN1 7
+#define IN2 8
+#define ENA 5
+#define RR A0
+
+
+Encoder motor(2,3); //2 interrupt pins
+const int oneRev=194;
+long currentEncoder=0;
+long previousEncoder=0;
+long currentMillis=0;
+long previousMillis=0;
+int interval=1000;  //ms
+int direct=1;
+
+void setup() {
+  pinMode(IN1,OUTPUT);
+  pinMode(IN2,OUTPUT);
+  pinMode(ENA,OUTPUT);
+  Serial.begin(9600);
+  Serial.println("Reading speed of the motor");
+  Serial.println("-----------------------------------------");
+}
+
+
+
+void loop() {
+  float angularSpeed=read_speed();
+  Serial.print("Angular speed=  \t");
+  Serial.println(angularSpeed);
+  
+}
+
+float read_speed(void)
+{
+    //read angular speed of the motor
+    //return angular speed in rpm
+    currentEncoder = motor.read();
+    currentMillis = millis();
+    if (currentMillis - previousMillis >= interval)
+    {
+        previousMillis = currentMillis;
+        float rot_speed = (float)((60000/interval)*(currentEncoder - previousEncoder)/oneRev);
+        previousEncoder = currentEncoder;
+        return rot_speed;
+    }
+    analogWrite(ENA,55);
+    digitalWrite(IN1,direct);
+    digitalWrite(IN2,!direct);
+}
